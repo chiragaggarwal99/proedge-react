fda722db548e96687051d784ad314ba6a5aefdfd
chiragaggarwal99 <99chiragaggarwal@gmail.com>
2020-12-13 01:10:58 +0530

    assignment 2

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Proedge ReactJS Assignment: 
 https://proedge-react.herokuapp.com/
 
+Assignment 2: 
+To shorten a string and expand it back
+
+Algorithm for shortening:<br>
+1. Assign numbers from (1 to 26) to (a to z) respectively.<br>
+2. Convert each number to its binary equivalent of 5-bits (because 2^5 > 26). <br>
+3. Concatenate all the binary into a single string.<br>
+4. Now take 6 bits at a time and convert it into its decimal equivalent (6 bits because 2^6 = 64 i.e. using only 64 characters from ASCII table to avoid unnecessary character like null, space etc.)<br>
+5. Add 58 to each decimal value to avoid first 58 characters of ASCII table.<br>
+6. Convert each decimal value to its character equivalent and concatenate into a single string to obtain the compressed data.<br>
+
+Assignment 1:
+To make an API call and display it in a table
+
 Project Structure:<br>
 index.js<br>
 &nbsp;&nbsp;&nbsp;&nbsp;-src/App.js<br>
--- src/components/Main.js
+++ src/components/Main.js
@@ -6,11 +6,16 @@ class Main extends Component {
     state = {
         data: [],
         loading: false,
-        text: ''
+        text: '',
+        compressionText: 'marvelcomics',
+        compressedText: '',
+        decompressionText: '',
+        decompressedText: '',
+        copied: false
     }
 
     submit = () => {
-        if(this.state.text == ""){
+        if(this.state.text === ""){
             alert('Invalid Input');
             return;
         }
@@ -28,37 +33,145 @@ class Main extends Component {
         });
     }
 
+    shorten = () => {
+        var reg = /^[a-z]+$/;
+        if (!reg.test(this.state.compressionText)) {
+            alert("Invalid input!");
+            return;
+        } 
+        var compressedInput = '';
+        var bitString = '';
+        for(let ch of this.state.compressionText){
+            let code = (ch.charCodeAt()-96); //coded as -> a=1,b=2,c=3... 
+            let bits = (code).toString(2);  //converted to bits -> a=1,b=10,c=11...
+            let fiveDigitBits = ("000000" + bits).substr(-5); // coverted to five digit bits -> a=00001,b=00010...
+            bitString += fiveDigitBits; // combined into single string
+        }
+
+        var n = 6; // number of bits used to create a single character of compressed string
+        for(let ch=0;ch<bitString.length;ch+=n){
+            let nDigitBits = bitString.substr(ch,n); // n bit characters
+            if(nDigitBits.length!==n){
+                // when ndigitbits is less than number of bits required, for example to convert 011 to 011000 (when n=6)
+                let temp = parseInt(nDigitBits,2)<<n-nDigitBits.length;
+                nDigitBits = ("0000000" + temp.toString(2)).substr(-6);
+            }
+            let code = parseInt(nDigitBits,2); // binary to decimal
+            let char = String.fromCharCode((code+58).toString(10)); // decimal+58 to char to avoid invalid characters
+            compressedInput += char; 
+        }
+        this.setState({ compressedText: compressedInput, copied: false });
+    }
+
+    expand = () => {
+        var decompressedOutput = '';
+        var bitString = '';
+
+        for(let ch of this.state.decompressionText){
+            let code = (ch.charCodeAt()-58); //decoded as decimal from character 
+            let bits = (code).toString(2);  //converted to bits -> a=1,b=10,c=11...
+            let fiveDigitBits = ("0000000" + bits).substr(-6); // coverted to six digit bits -> a=000001,b=000010...
+            bitString += fiveDigitBits; // combined into single string
+        }
+        
+        var n = 5; // number of bits used to create a single character of decompressed string
+        for(let ch=0;ch<bitString.length;ch+=n){
+            let nDigitBits = bitString.substr(ch,n); // n bit characters
+            let code = parseInt(nDigitBits,2); // binary to decimal
+            if(code===0)break; // last remaining zero bits are null i.e. not required
+            let char = String.fromCharCode((code+96).toString(10)); // decimal+96 to recover original characters
+            decompressedOutput += char;
+        }
+        this.setState({ decompressedText: decompressedOutput });
+    }
+
+    copy = () => {
+        navigator.clipboard.writeText(this.state.compressedText);
+        this.setState({ copied: true });
+    }
+
+    compressionTextChanged = (event) => {
+        this.setState({ compressionText: event.target.value, compressedText:'' })
+    }
+
     render(){       
         const { data, loading } = this.state;
         return (
             <div className="Main">
-                <div>
-                    <span>Enter comma seperated Roll Numbers:</span>
-                    <input 
-                        className="input" 
-                        type="text" 
-                        onChange={event => this.setState({ text: event.target.value }) } 
-                        value={this.state.text}
-                        />
-                    { loading ? <div className="loader"></div> : <button onClick={this.submit}>Submit</button>}
-                </div>
-                
-                { data.length !==0 && 
+                <div className="assignment-section">
+                    <h1>Assignment 1</h1>
                     <div>
-                        <h2>Result:</h2>
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th>Roll No.</th>
-                                    <th>Result</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {data.map(a => <tr key={a.rollNo}><td>{a.rollNo}</td><td>{a.result}</td></tr>)}
-                            </tbody>
-                        </table>
+                        <span>Enter comma seperated Roll Numbers:</span>
+                        <input 
+                            className="input" 
+                            type="text" 
+                            onChange={event => this.setState({ text: event.target.value }) } 
+                            value={this.state.text}
+                            />
+                        { loading ? <div className="loader"></div> : <button onClick={this.submit}>Submit</button>}
                     </div>
-                }
+                    
+                    { data.length !==0 && 
+                        <div>
+                            <h2>Result:</h2>
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th>Roll No.</th>
+                                        <th>Result</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {data.map(a => <tr key={a.rollNo}><td>{a.rollNo}</td><td>{a.result}</td></tr>)}
+                                </tbody>
+                            </table>
+                        </div>
+                    }
+                </div>
+
+                <div className="assignment-section">
+                    <h1>Assignment 2</h1>
+                    <div className="row">
+                        <div className="col">
+                            <span>Enter string (a-z) to shorten:</span>
+                            <input 
+                                className="input" 
+                                type="text" 
+                                onChange={event => this.compressionTextChanged(event) } 
+                                value={this.state.compressionText}
+                                />
+                            <button onClick={this.shorten}>Shorten</button>
+                            { this.state.compressedText!=='' && 
+                                <>
+                                    <div className="data">
+                                        <span>Compressed Data: {this.state.compressedText}</span>
+                                        { this.state.copied ? <span>Copied!</span> : <button onClick={this.copy}>Copy</button> }
+                                    </div>
+                                    <p>Data Length: {this.state.compressionText.length}</p>
+                                    <p>Compressed Data Length: {this.state.compressedText.length}</p>
+                                </>
+                            }
+                        </div>
+                        <div className="col">
+                            <span>Enter string to expand back:</span>
+                            <input 
+                                className="input" 
+                                type="text" 
+                                onChange={event => this.setState({ decompressionText: event.target.value }) } 
+                                value={this.state.decompressionText}
+                                />
+                            <button onClick={this.expand}>Expand</button>
+                            { this.state.decompressedText!=='' && 
+                                <>
+                                    <div>
+                                        <p>Decompressed Data: {this.state.decompressedText}</p>
+                                    </div>
+                                    <p>Decompressed Data Length: {this.state.decompressedText.length}</p>
+                                </>
+                            }
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
